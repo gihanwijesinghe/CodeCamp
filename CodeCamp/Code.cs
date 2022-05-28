5a7af14f12a5cb71c7ae2b6d827ec7f10a220289
Gihan Wijesinghe <gihanwijesinghe12@gmail.com>
2022-05-29 03:47:25 +0530

    adding execute method

---
+++ CodeCamp/Code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCamp
+{
+    public class Code
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Execute");
+        }
+    }
+}
--- CodeCamp/Program.cs
+++ CodeCamp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodeCamp;
+
 Console.WriteLine("Hello, World!");
+
+var code = new Code();
+code.Execute();
