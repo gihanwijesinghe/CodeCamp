ff4a2b915327eb9f78d5b754f5788b07d6b18f8e
Gihan Wijesinghe <gihanwijesinghe12@gmail.com>
2022-05-29 06:42:15 +0530

    is prime number check

--- CodeCamp/Algorithms.cs
+++ CodeCamp/Algorithms.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeCamp
 {
-    public class Alogorithms
+    public class Algorithms
     {
         public Dictionary<int, int> GetPrimeFactors(int num)
         {
@@ -48,6 +48,50 @@ namespace CodeCamp
             }
 
             return primefactors;
+        }
+
+        public bool IsPrimeNumber(int num)
+        {
+            var value = num;
+
+            var divider = 2;
+            var nextDivider = value / 2;
+            //var primefactors = new Dictionary<int, int>();
+
+            while (divider <= nextDivider)
+            {
+                if (value % divider == 0)
+                {
+                    return false;
+                    //if (primefactors.ContainsKey(divider))
+                    //{
+                    //    primefactors[divider] = primefactors[divider] + 1;
+                    //}
+                    //else
+                    //{
+                    //    primefactors.Add(divider, 1);
+                    //}
+                    //value = value / divider;
+                    //nextDivider = value / divider;
+                }
+                else
+                {
+                    nextDivider = value / divider;
+                    divider++;
+                }
+            }
+
+            return true;
+            //if (primefactors.ContainsKey(value))
+            //{
+            //    primefactors[value] = primefactors[value] + 1;
+            //}
+            //else
+            //{
+            //    primefactors.Add(value, 1);
+            //}
+
+            //return primefactors;
         }
 
         public int GetLeastCommonMultiple(int[] array)
