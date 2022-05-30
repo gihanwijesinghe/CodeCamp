e6c6cddaf704f46f2bd38fde2e0620dd8ca69810
Gihan Wijesinghe <gihanwijesinghe12@gmail.com>
2022-05-30 06:49:00 +0530

    result

--- CodeCamp/Code.cs
+++ CodeCamp/Code.cs
@@ -10,7 +10,49 @@ namespace CodeCamp
     {
         public void Execute()
         {
+            var stringSeparated = "4 3 2 1";
+            var q = new List<int>();
+            foreach (var item in stringSeparated.Split(" "))
+            {
+                q.Add(int.Parse(item.ToString()));
+
+            }
+
             Console.WriteLine("Execute  ");
+
+            var total = 0;
+
+            var previou = q[0];
+            var groupCount = 0;
+            for(var i = 0; i < q.Count; i++)
+            {
+                if(previou-q[i]  == 1)
+                {
+                    groupCount++;
+                }
+                else 
+                {
+                    if(groupCount > 0)
+                    {
+                        var innerCout = ((groupCount + 1) * (groupCount + 2) / 2)-(groupCount+1);
+                        total += innerCout;
+                        groupCount = 0;
+                    }
+                    
+                    
+                }
+                previou = q[i];
+            }
+
+            if (groupCount > 0)
+            {
+                var innerCout = ((groupCount + 1) * (groupCount + 2) / 2) - (groupCount + 1);
+                total += innerCout;
+            }
+
+            total += q.Count();
+
+            Console.WriteLine("total  "+total);
         }
     }
 }
